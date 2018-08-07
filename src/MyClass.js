22d62abc6bb0925964dc0e680106ea0679e93945
Michael Cherry-Leigh <mcherryleigh@gmail.com>
2018-08-07 18:49:47 -0400

    adding eslint and correcting files where it was necessary, plugging eslint into Travis where it should currently fail builds if error level rules are broken

--- src/MyClass.js
+++ src/MyClass.js
@@ -3,6 +3,11 @@
  * @desc A class description
  */
 module.exports = class MyClass {
+  constructor() {
+    // airbnb thinks I shouldn't have class methods without calling `this`. Here's a dumb example
+    this.counter = 0;
+  }
+
   /**
    * @access public
    * @desc A method description
@@ -13,8 +18,8 @@ module.exports = class MyClass {
    * @param {number} b - this is a value.
    * @return {number} result of the sum value.
    */
-  sum(a, b){
+  sum(a, b) {
+    this.tempVar += 1;
     return a + b;
   }
-}
-
+};
--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 import test from 'ava';
-import MyClass from '../src/MyClass'
+import MyClass from '../src/MyClass';
 
 /** @test {MyClass.sum} */
-test('1+1=2', async t => {
-  let calc  = new MyClass()
-  let sum = calc.sum(1,1)
+test('1+1=2', async (t) => {
+  const calc = new MyClass();
+  const sum = calc.sum(1, 1);
   t.is(sum, 2);
 });
